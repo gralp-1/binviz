5fe075bbb97e6988a4503df8f3d8c624fd3a4fa5
gralp <don't email me>
2023-12-23 18:42:15 +0000

    had to code this twice

--- README.md
+++ README.md
@@ -11,11 +11,10 @@ Before the image is created, the data is normalized to the range 0-255 with an a
 Each consecutive triplet of bytes is a coordinate to a 256x256x256 point cloud. The point cloud counts the occurance of each triplet of bytes which is used to represent the brightness of the pixel at that coordinate.
 
 ## How do I use it?
-```
+```sh
 go build .
 ./binviz -file <input file> -output <output name> -dimension <dimension> -brightness <brightness> # output name, brightness and dimension are optional
 ```
-This will produce either a PPM image file or a PCD point cloud file depending on the dimension argument.
 
 ## Examples
 Input: <br>
@@ -27,36 +26,20 @@ Output 3D: <br>
 
 ## File format
 I can't find any file formats that support 4 dimensions, so time to make my own. <br>
-I don't know what I'm going to call it yet and I also have no idea what I'm doing. <br>
-This format is WIP and will have the following features:
-- 2D, 3D and 4D support
-- Colour and greyscale support
+The file format is very simple, it's just a header followed by a list of points. It takes inspiration from the [.ppm](https://netpbm.sourceforge.net/doc/ppm.html) and [.pcd](https://pointclouds.org/documentation/tutorials/pcd_file_format.html) formats. <br>
 
 ### Example
 ```
-// Header (always in ascii)
-VERSION <float>
+VERSION <semver string>
 DIMENSION <2 | 3 | 4>
-COLOUR <true | false>
-ENCODING <ASCII | BINARY>
-// File start
-x y r g b     // 2D ASCII colour
-x y val       // 2D ASCII greyscale
-x y z r g b   // 3D ASCII colour
-x y z val     // 3D ASCII greyscale
-x y z w r g b // 4D ASCII colour
-x y z w val   // 4D ASCII greyscale
+x y val       // 2D
+x y z val     // 3D
+x y z w val   // 4D
 ```
-Each line is delimited by a newline character. <br>
-Comments are added with `//` and are ignored by the parser. <br>
-all fields must be from 0-255. <br>
-If encoded in binary, each field is one byte and everything is just assumed to be in the correct order, if bytes are missing / there are extra bytes, you'll just get something strange in the visualzer <br>
-For example a point at (34,35,69,255) with a colour of (20,40,10) would be encoded as `34 35 69 20 40 10` in ASCII and `22 23 45 14 28 0A` in binary.
+All fields must be from 0-255. <br>
 
 # TODO
 - [x] Refactor to make consistent
 - [x] Add proper command line flags & arguments
-- [ ] Look into 4D file formats and visualisation
-- [ ] Make a custom visualiser and file format which supports 2, 3 and 4 dimensions
-- [ ] Make a way to change the brightness adjustment function
-- [ ] Optimise
+- [ ] Adjustable brightness curves
+- [ ] Make vizualiser
--- main.go
+++ main.go
@@ -10,18 +10,36 @@ import (
 
 // global to "flatten out" data transfer
 var modifier *float64
+var outputFileName *string
+var dimension *int
+
+const version string = "0.0.1"
+
+type Point struct {
+	Coordinate []uint8 // X, Y, Z, W
+	Value      uint8
+}
+
+func (p *Point) ToString() string {
+	coordinateString := ""
+	for _, v := range p.Coordinate {
+		coordinateString += fmt.Sprintf("%d ", v)
+	}
+	return fmt.Sprintf("%s%d\n", coordinateString, p.Value)
+}
 
 func main() {
 	// Command line arguments
 	fileName := flag.String("file", "", "Input file name")
-	outputName := flag.String("output", "output.ppm", "Output file name")
-	dimension := flag.Int("dimension", 2, "Dimensionality of output (2 or 3)")
+	outputFileName = flag.String("output", "output.viz", "Output file name")
+	dimension = flag.Int("dimension", 2, "Dimensionality of output (2 or 3)")
 	modifier = flag.Float64("brightness", 1.0, "Brightness modifier")
+	// TODO check these arguments for validity
 	flag.Parse()
 
 	// log arguments
 	fmt.Println("File:       ", *fileName)
-	fmt.Println("Output:     ", *outputName)
+	fmt.Println("Output:     ", *outputFileName)
 	fmt.Println("Dimension:  ", *dimension)
 	fmt.Println("Brightness: ", *modifier)
 	file, err := os.OpenFile(*fileName, os.O_RDONLY, 0666)
@@ -40,128 +58,63 @@ func main() {
 	}
 
 	// call appropriate function
-	switch *dimension {
-	case 2:
-		var pixels = Binviz2D(fileBytes)
-		WritePPM(pixels, *outputName)
-	case 3:
-		var pixels = Binviz3D(fileBytes)
-		WritePCD(pixels, *outputName)
-	default:
-		log.Fatal("Invalid dimensionality")
-	}
-}
-func AdjustBrightness(pixel float64) float64 {
-	// some good 'ol dereferencing in the middle of an equation for ya
-	return math.Min(1.0-math.Pow((1.0-(*modifier)*pixel), 5.0), 1)
-}
-func AdjustBrightnessSigmoid(pixel float64) float64 {
-	return math.Pow(1+math.Pow(10, -20*pixel), -10)
-}
-
-func Binviz3D(fileBytes []byte) [256][256][256]int {
-	// initialise 256x256 array with 0s
-	var pixels [256][256][256]int = [256][256][256]int{}
-	// count number of occurrences of each byte pair
-	for i := 0; i < len(fileBytes)-2; i++ {
-		b1 := fileBytes[i]
-		b2 := fileBytes[i+1]
-		b3 := fileBytes[i+2]
-		pixels[b1][b2][b3]++
-	}
-	// find max value
-	max := 0
-	for i := 0; i < 256; i++ {
-		for j := 0; j < 256; j++ {
-			for k := 0; k < 256; k++ {
-				if pixels[i][j][k] > max {
-					max = pixels[i][j][k]
-				}
-			}
-		}
-	}
-	// normalise values
-	for i := 0; i < 256; i++ {
-		for j := 0; j < 256; j++ {
-			for k := 0; k < 256; k++ {
-				pixels[i][j][k] = int(AdjustBrightness(float64(pixels[i][j][k])/float64(max)) * 255)
-			}
-		}
-	}
-	return pixels
-}
-func WritePCD(pixels [256][256][256]int, fName string) error {
-	f, err := os.OpenFile(fName, os.O_CREATE|os.O_WRONLY, 0666)
+	points := Binviz(fileBytes)
+	err = Serialise(points)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	if err := f.Truncate(0); err != nil {
-		return err
-	}
-
-	f.WriteString("VERSION 0.7\n")
-	f.WriteString("FIELDS x y z rgb\n")
-	f.WriteString("SIZE 4 4 4 4\n")
-	f.WriteString("TYPE I I I I\n")
-	f.WriteString("COUNT 1 1 1 1\n")
-	f.WriteString("WIDTH 256\n")
-	f.WriteString("HEIGHT 256\n")
-	f.WriteString("VIEWPOINT 0 0 0 1 0 0 0\n")
-	f.WriteString("POINTS 65536\n")
-	f.WriteString("DATA ascii\n")
-	for x := 0; x < 256; x++ {
-		for y := 0; y < 256; y++ {
-			for z := 0; z < 256; z++ {
-				val := pixels[x][y][z]
-				if val != 0 {
-					f.WriteString(fmt.Sprintf("%d %d %d %d %d %d\n", x, y, z, val, val, val))
-				}
-			}
-		}
-	}
-	return nil
 }
 
-func Binviz2D(fileBytes []byte) [256][256]int {
-	// initialise 256x256 array with 0s
-	var pixels [256][256]int = [256][256]int{}
+func Binviz(fileBytes []byte) []Point {
+	// similar to Binviz2D, but with variable dimensionality
+	// 0xXXYYZZWW : frequency
+	var freqencyMap map[uint32]int = make(map[uint32]int)
 	// count number of occurrences of each byte pair
-	for i := 0; i < len(fileBytes)-1; i++ {
-		b1 := fileBytes[i]
-		b2 := fileBytes[i+1]
-		pixels[b1][b2]++
+	for i := 0; i < len(fileBytes)-*dimension; i++ {
+		var key uint32 = 0
+		for j := 0; j < *dimension; j++ {
+			key = key << 8
+			key += uint32(fileBytes[i+j])
+		}
+		freqencyMap[key]++
 	}
 	// find max value
 	max := 0
-	for i := 0; i < 256; i++ {
-		for j := 0; j < 256; j++ {
-			if pixels[i][j] > max {
-				max = pixels[i][j]
-			}
+	for _, v := range freqencyMap {
+		if v > max {
+			max = v
 		}
 	}
 	// normalise values and adjust brightness
-	for i := 0; i < 256; i++ {
-		for j := 0; j < 256; j++ {
-			pixels[i][j] = int(AdjustBrightness(float64(pixels[i][j])/float64(max)) * 255)
+	var points []Point = make([]Point, len(freqencyMap))
+	i := 0
+	for k, v := range freqencyMap {
+		points[i].Coordinate = make([]uint8, *dimension)
+		for j := 0; j < *dimension; j++ {
+			points[i].Coordinate[*dimension-j-1] = uint8(k >> (8 * j))
 		}
+		points[i].Value = uint8(AdjustBrightness(float64(v)/float64(max)) * 255)
+		i++
 	}
-	// write to file
-	return pixels
+	return points
 }
-func WritePPM(pixels [256][256]int, fName string) error {
-	f, err := os.OpenFile(fName, os.O_CREATE|os.O_WRONLY, 0666)
+func Serialise(points []Point) error {
+	// Write header
+	f, err := os.OpenFile(*outputFileName, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return err
 	}
-	if err := f.Truncate(0); err != nil {
-		return err
-	}
-	f.WriteString("P3\n256 256\n255\n")
-	for i := 0; i < 256; i++ {
-		for j := 0; j < 256; j++ {
-			f.WriteString(fmt.Sprintf("%d %d %d\n", pixels[i][j], pixels[i][j], pixels[i][j]))
-		}
+	defer f.Close()
+	f.WriteString(fmt.Sprintln("VERSION", version))
+	f.WriteString(fmt.Sprintln("DIMENSION", *dimension))
+	// Write points
+	for _, p := range points {
+		f.WriteString(p.ToString())
 	}
 	return nil
+}
+
+func AdjustBrightness(pixel float64) float64 {
+	// some good 'ol dereferencing in the middle of an equation for ya
+	return math.Min(1.0-math.Pow((1.0-(*modifier)*pixel), 5.0), 1)
 }
